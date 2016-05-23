e22139f7a32063cdd7f7b6103e9283da6f14b389
srikanthpenmetsa <penmetsa.srikanth@gmail.com>
2016-05-23 15:16:10 +0530

    Add files via upload

---
+++ Tasko.Model/LoginInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasko.Model
+{
+    /// <summary>
+    /// LoginInfo Class
+    /// </summary>
+    [DataContract]
+    public class LoginInfo
+    {
+        /// <summary>
+        /// Gets or sets the token identifier.
+        /// </summary>
+        /// <value>
+        /// The token identifier.
+        /// </value>
+        [DataMember]
+        public string TokenId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user identifier.
+        /// </summary>
+        /// <value>
+        /// The user identifier.
+        /// </value>
+        [DataMember]
+        public string UserId { get; set; }
+    }
+}
