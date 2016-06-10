69bba6cdd294a0a3052e432ec09bc016e6cc9af9
mchandu123 <mchandu123@gmail.com>
2016-06-10 18:00:46 +0530

    Update OrderSummary.cs

--- Tasko.Model/OrderSummary.cs
+++ Tasko.Model/OrderSummary.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -56,7 +56,7 @@ namespace Tasko.Model
         /// The requested date.
         /// </value>
         [DataMember]
-        public DateTime RequestedDate { get; set; }
+        public string RequestedDate { get; set; }
 
         /// <summary>
         /// Gets or sets the comments.
