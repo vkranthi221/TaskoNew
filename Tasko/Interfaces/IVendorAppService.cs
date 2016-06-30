444b3b561acfd295afb11783b231bb018d2dee8a
penmetsa.srikanth <penmetsa.srikanth@gmail.com>
2016-06-30 21:23:28 +0530

    Made changes for api documentation

--- Tasko/Interfaces/IVendorAppService.cs
+++ Tasko/Interfaces/IVendorAppService.cs
@@ -10,10 +10,31 @@ namespace Tasko.Interfaces
     [ServiceContract]
     public interface IVendorAppService
     {
-        /// <summary>
-        /// Gets the token.
-        /// </summary>
-        /// <returns>Response Object</returns>
+        /**
+         * @api {post} v1/GetAuthCode Get Auth Code
+         * @apiName GetAuthCode
+         * @apiGroup Vendor
+         * 
+         * @apiParam\n
+         *
+         * @apiSuccessExample Success-Response:
+         {
+          "Data": "3E71AF3FF2196045BB3212623BFDEF91",
+         },
+          "Error": false,
+          "Message": "Authentication Successful",
+          "Status": 200
+        }
+         * @apiError Authentication failed.
+         *
+         * @apiErrorExample Error-Response:
+         {
+          "Data": null,
+          "Error": true,
+          "Message": "Authentication failed",
+          "Status": 400
+        }
+         */
         [OperationContract]
         [FaultContract(typeof(ErrorDetails))]
         [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
@@ -21,19 +42,71 @@ namespace Tasko.Interfaces
             UriTemplate = "auth")]
         Response GetAuthCode();
 
-        /// <summary>
-        /// Logins the specified username.
-        /// </summary>
-        /// <param name="username">The username.</param>
-        /// <param name="password">The password.</param>
-        /// <param name="mobilenumber">The mobilenumber.</param>
-        /// <returns>Response Object</returns>
+        /**
+         * @api {post} v1/Login Vendor Login
+         * @apiName Login
+         * @apiGroup Vendor
+         * 
+         * @apiParam {String} username User name to login.  
+         * 
+         * @apiParam {String} password password of the user.  
+         * 
+         * @apiParam {String} mobilenumber mobile number of the user. 
+         * 
+         * @apiParam {Int16} userType user type. 
+         *
+         * @apiSuccessExample Success-Response:
+         {
+          "Data": {
+                     "__type": "LoginInfo:#Tasko.Model",
+                     "TokenId": "6CEC15B7B6FC23439AC131F54551BB22",
+                     "UserId": "FC73EC7242E28142ACCAFDF4703F0EBF"
+                  }
+         },
+          "Error": false,
+          "Message": "Login Successfull",
+          "Status": 200
+        }
+         * @apiError Authentication failed.
+         *
+         * @apiErrorExample Error-Response:
+         {
+          "Data": null,
+          "Error": true,
+          "Message": "Authentication failed",
+          "Status": 400
+        }
+         */
         [OperationContract]
         [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         Response Login(string username, string password, string mobilenumber, Int16 userType);
 
-
+        /**
+         * @api {post} v1/Logout Log out
+         * @apiName Logout
+         * @apiGroup Vendor
+         * 
+         * @apiParam\n
+         *
+         * @apiSuccessExample Success-Response:
+         {
+          "Data": "null",
+         },
+          "Error": false,
+          "Message": "Success",
+          "Status": 200
+        }
+         * @apiError Authentication failed.
+         *
+         * @apiErrorExample Error-Response:
+         {
+          "Data": null,
+          "Error": true,
+          "Message": "Error logging out",
+          "Status": 400
+        }
+         */
         [OperationContract]
         [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.WrappedRequest)]
@@ -82,11 +155,38 @@ namespace Tasko.Interfaces
             BodyStyle = WebMessageBodyStyle.WrappedRequest)]
         Response GetVendorDetails(string vendorId);
 
-        /// <summary>
-        /// Gets the vendor services.
-        /// </summary>
-        /// <param name="vendorId">The vendor identifier.</param>
-        /// <returns>Response Object</returns>
+        /**
+         * @api {post} v1/GetVendorServices Get Vendor services
+         * @apiName GetVendorServices
+         * @apiGroup Vendor
+         *
+         * @apiParam {String} vendorId Vendor Id.                
+         *
+         * @apiSuccessExample Success-Response:
+         {
+          "Data": [
+                    {
+                        "__type": "VendorService:#Tasko.Model",
+                         "Id": "CF9A27B3DA0D5E418B1A8E6CC79218AD",
+                         "ImageURL": "",
+                         "IsActive": true,
+                         "Name": "Microwave Service"
+                    }
+                 ]
+          "Error": false,
+          "Message": "Success",
+          "Status": 200
+        }
+         * @apiError NO_SERVICES_AVAILABLE No services available.
+         *
+         * @apiErrorExample Error-Response:
+         {
+          "Data": null,
+          "Error": true,
+          "Message": "No services available",
+          "Status": 400
+        }
+         */
         [OperationContract]
         [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Json,
             BodyStyle = WebMessageBodyStyle.WrappedRequest)]
