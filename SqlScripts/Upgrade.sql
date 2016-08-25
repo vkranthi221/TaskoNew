815c8e916087035fc4258cefd02a4c917669f93f
mchandu123@gmail.com <mchandu123@gmail.com>
2016-08-25 21:16:20 +0530

    Updated the GetServiceVendors SP

--- SqlScripts/Upgrade.sql
+++ SqlScripts/Upgrade.sql
@@ -44,3 +44,33 @@ UPDATE SERVICES SET IMAGE_URL = 'http://api.tasko.in/serviceimages/Bike_Service.
 UPDATE SERVICES SET IMAGE_URL = 'http://api.tasko.in/serviceimages/ac_services.png' where NAME = 'AC Installation'
 
 GO
+
+ALTER PROCEDURE [dbo].[usp_GetServiceVendors]
+(
+  @pServiceId binary(16),
+  @pCustomerId binary(16)
+)
+
+AS
+BEGIN
+
+--            CustomerRequested = 1,
+--            VendorAccepted = 2,
+--            VendorRejected = 3,
+--            CustomerAccepted = 4,
+--            OrderCompleted = 5,
+--            CustomerCancelled = 6,
+--            VendorCancelled = 7,
+
+SET NOCOUNT ON;
+	SELECT SVC.SERVICE_ID ,SVC.NAME AS SERVICE_NAME, V.VENDOR_ID, V.NAME AS VENDOR_NAME,VENDOR_SERVICE_ID, V.BASE_RATE, CV.FAVORITE_ID,
+	(SELECT COUNT(*) FROM VENDOR_RATING VR WHERE VR.VENDOR_ID = V.VENDOR_ID)AS TOTAL_REVIEWS, dbo.[GetVendorTotalRating](V.VENDOR_ID) AS OVERALL_RATINGS
+	FROM [dbo].[SERVICES] SVC
+	INNER JOIN [dbo].[VENDOR_SERVICES] VS ON VS.SERVICE_ID = SVC.SERVICE_ID
+	INNER JOIN [dbo].[VENDOR] V ON V.VENDOR_ID = VS.VENDOR_ID
+	LEFT OUTER JOIN [dbo].[CUSTOMER_FAVORITE_VENDOR] CV ON CV.CUSTOMER_ID = @pCustomerId AND CV.VENDOR_ID = V.VENDOR_ID
+	WHERE VS.SERVICE_ID = @pServiceId AND VS.IS_VENDOR_SERVICE_ACTIVE = 1  
+	AND VS.VENDOR_SERVICE_ID NOT IN(SELECT VENDOR_SERVICE_ID FROM [ORDER] WHERE ORDER_STATUS_ID IN(1,2,4))
+END
+
+GO
