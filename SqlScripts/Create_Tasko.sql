8e9e9be94597c5ad8c9c456093e1363440943921
mchandu123 <mchandu123@gmail.com>
2016-11-08 18:25:48 +0530

    Update Create_Tasko.sql

--- SqlScripts/Create_Tasko.sql
+++ SqlScripts/Create_Tasko.sql
@@ -2817,3 +2817,26 @@ BEGIN
 END
 
 GO
+CREATE PROCEDURE [dbo].[usp_IsVendorExists]
+(
+	@pUsername varchar(max),
+	@pMobileNumber varchar(50),
+	@pEmailAddress varchar(max) 
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	DECLARE @count int
+	DECLARE @IsVendorExists bit
+	SELECT @count = COUNT(1) from VENDOR V WHERE V.USER_NAME = @pUsername OR MOBILE_NUMBER = @pMobileNumber OR EMAIL_ADDRESS = @pEmailAddress
+	IF(@count >0)
+      BEGIN
+	    SET @IsVendorExists = 1
+	  END
+	ELSE
+	  BEGIN 
+	     SET @IsVendorExists =0
+	  END
+	SELECT @IsVendorExists as IsVendorExists
+END
+GO
