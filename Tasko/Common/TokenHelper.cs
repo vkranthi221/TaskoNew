60f169b149453876b3884496fa35d80750bbdd15
mchandu123@gmail.com <mchandu123@gmail.com>
2016-11-26 14:45:04 +0530

    file missing

---
+++ Tasko/Common/TokenHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.Web;
+using System.Web;
+using Tasko.Repository;
+
+namespace Tasko.Common
+{
+    public static class TokenHelper
+    {
+        /// <summary>
+        /// Validates the token.
+        /// </summary>
+        /// <returns>bool value</returns>
+        public static bool ValidateToken()
+        {
+            IncomingWebRequestContext request = WebOperationContext.Current.IncomingRequest;
+            WebHeaderCollection headers = request.Headers;
+            string tokenCode = headers["Token_Code"];
+            string userId = headers["User_Id"];
+            if (!string.IsNullOrEmpty(tokenCode) && !string.IsNullOrEmpty(userId))
+            {
+                bool isTokenValid = VendorData.ValidateTokenCode(tokenCode, userId);
+                return isTokenValid;
+            }
+
+            return false;
+        }
+    }
+}
